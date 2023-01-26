54154563897a026be93ee0668a28ffd515afc42e
dell <gauravdevgd2000@gmail.com>
2023-01-25 20:15:10 -0800

    percentage and cube

---
+++ src/variables/percentageAndcube.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class percentageAndcube {
+    public static void main(String[] args){
+        int a= 24;
+        int b= 43;
+        int c= 45;
+        int number= 24;
+        int percentage= 2;
+        double result= (double) (number*percentage)/ 100;
+        System.out.println("24 of 2% : " +result);
+
+
+
+
+
+
+    }
+}
