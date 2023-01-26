c0b096afa1298ed3be90c540a4a90236d514c712
dell <gauravdevgd2000@gmail.com>
2023-01-26 01:42:58 -0700

    Assingment25jan2

---
+++ src/variables/Assingnment25jan2.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Assingnment25jan2 {
+    public static void main(String[] args){
+        //cube and percentage// ...  //
+        int a= 5;
+        int cube= a*a*a;
+        System.out.println("Cube is : " +cube);
+        int number= 34550;
+        int percentage= 5;
+        double result= (double) (number*percentage)/100;
+        System.out.println("Percentage is : " +result);
+
+
+    }
+}
