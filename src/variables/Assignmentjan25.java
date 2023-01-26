d777a104500301cf1db7c38e204c1b352619df54
dell <gauravdevgd2000@gmail.com>
2023-01-25 20:49:37 -0800

    Assingment25jan

---
+++ src/variables/Assignmentjan25.java
@@ -0,0 +1,22 @@
+package variables;
+
+public class Assignmentjan25 {
+    public static void main(String[] args){
+        //Program to print Country,Population,Number of State//
+        String country="India";
+        String population= "1.42 Billion";
+        String noOfStates= "28";
+        System.out.println("Country is  : " +country);
+        System.out.println("Population is : " +population);
+        System.out.println("No Of Country is : " +noOfStates);
+
+
+        //task2//
+        double Namepopulation= 1.42;
+        System.out.println("Population is : " +Namepopulation+ (" billion.."));
+        int NamenoofCountry= 28;
+        System.out.println("No Of States is : " +NamenoofCountry+ (" States"));
+
+
+    }
+}
