5a0ca64062848a23f4ae220914f1ae9db700ebdb
dell <gauravdevgd2000@gmail.com>
2023-01-27 21:56:42 -0700

    Assingnment27jan if condition statement

---
+++ src/conditional_statement/IfAssingnment27jan.java
@@ -0,0 +1,70 @@
+package conditional_statement;
+
+public class IfAssingnment27jan {
+    public static void main(String[] args) {
+        int age = 19;
+        if (age > 18) {
+            System.out.println("Person eligible for voting");
+            String city = "Agra";
+            if (city == "Agra") {
+                System.out.println("Person beloges to Agra");
+                int number = 67;
+                if (number >= 21) {
+                    System.out.println("67 to greater than 21");
+                    String city2 = "Pune";
+                    if (city2 != "Agra") {
+                        System.out.println("Person Belongs To Some Other City");
+                    }
+                    //Assingnment27jan start//
+
+
+                    int marks = 60;
+                    if (marks > 55) {
+                        System.out.println("Student clear exam with first division");
+                        if (marks > 55) {
+                            System.out.println("Student cleared exam with second division");
+                            if (marks > 33) {
+                                System.out.println("Student cleard exam with passing markes");
+
+                                int marks2 = 55;
+                                if (marks2 > 33) {
+                                    System.out.println("Student cleard exam with second division");
+                                    int marks3 = 33;
+                                    if (marks >= 33) {
+                                        System.out.println("Student cleard exam with passing marks");
+                                        int age2 = 60;
+                                        if (age2 > 45) {
+                                            System.out.println("Person is old");
+                                            int age3= 45;
+                                            if(age3>18){
+                                                System.out.println("Person is Young");
+                                                int age4= 18;
+                                                if(age4==18){
+                                                    System.out.println("Person is Child");
+                                                }
+                                            }
+
+                                            }
+
+
+
+
+                                            }
+
+
+                                        }
+                                    }
+
+
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+            }
+        }
+
+
+
---
+++ src/variables/JavaAdvance.java
@@ -0,0 +1,52 @@
+package variables;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+public class JavaAdvance {
+    public static void main(String[] args) {
+        //Advance Level For Java//
+        //byte - 1[-128 to 127]
+        //short - 2
+        //int - 4
+        //long - 8
+        //float - 4
+        //double - 8
+        //char - 2
+        //boolean - 1(true) and 0(false)
+        long phone = 7455072984L;
+        System.out.println("My Mobile No is : " + phone);
+        float pi = 3.14F;
+        System.out.println("Pi Value is : " + pi);
+        byte age = 22;
+        System.out.println("My Age is : " + age);
+        int a= 345;
+        int b= 234;
+        int multi= a*b;
+        System.out.println("Multi of : " +multi);
+        int square= a*a;
+        System.out.println("Square is : " +square);
+        double divide= (double) a/b;
+        System.out.println("Divide is : " +divide);
+        double number= 345;
+        double percentage= 25;
+        double result= (number*percentage)/100;
+        System.out.println("Percent is : " +result);
+        long phone2= 7055479272L;
+        System.out.println("My second no. is : " +phone2);
+        float gravity= 9.8F;
+        System.out.println("Gravity is : " +gravity);
+        int cube= a*a*a;
+        System.out.println("Cube is : " +cube);
+        String Hacker= "Gaurav";
+        System.out.println("Hacker is :" +Hacker);
+        double remainder= (double) b%a/100;
+        System.out.println("Remainder is : " +remainder);
+        boolean isAdult= true;
+        System.out.println("My age is : " +age+isAdult);
+
+
+
+
+
+    }
+}
