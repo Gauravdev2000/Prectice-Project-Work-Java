9037ba3c9b837edacde49c739304d6f611680df5
dell <gauravdevgd2000@gmail.com>
2023-01-29 01:08:33 -0700

    Assingnment28jan if and else condition statement

---
+++ src/conditional_statement/IforElesConditonalState.java
@@ -0,0 +1,31 @@
+package conditional_statement;
+
+public class IforElesConditonalState {
+    public static void main(String[] args){
+        String country= "America";
+        if(country=="America"){
+
+        }
+        if(country=="India"){
+            System.out.println("Indian");
+
+        }
+        else{
+            System.out.println("Foreigner");
+         int number= 25;
+         if(number%5 ==0){
+             System.out.println("This no. is divide by 5");
+         }
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
