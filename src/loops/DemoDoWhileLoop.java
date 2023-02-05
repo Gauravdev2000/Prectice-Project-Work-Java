c817645c76a2479455844d2bcdc94beee2c2a675
dell <gauravdevgd2000@gmail.com>
2023-02-05 06:51:09 -0700

    Demo While and Do While Loops .....

---
+++ src/loops/DemoDoWhileLoop.java
@@ -0,0 +1,23 @@
+package loops;
+
+public class DemoDoWhileLoop {
+    public static void main(String[] args) {
+        //syntax ;
+        //do{
+        // statement}
+        //while(condition);
+        int i=1;
+        do{
+            System.out.println("This is Do while loop demo" +i);
+            i++;
+        } while(i<5);
+        int j=5;
+        do{
+            System.out.println("This id do while decrement loop" +j);
+            j--;
+        }while(j>0);
+
+
+
+    }
+}
---
+++ src/loops/DemoWhileloop.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class DemoWhileloop {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<10){
+            System.out.println("While loop" +i);
+            i++;
+        }
+        int j=2;
+        while(j>0){
+            System.out.println("decrement loop" +j);
+            j--;
+        }
+
+
+
+    }
+}
