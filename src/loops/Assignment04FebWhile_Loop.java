27186c66549f8533cd4073ae65a03e172f2d66b3
dell <gauravdevgd2000@gmail.com>
2023-02-05 06:54:25 -0700

    Assignment04FebWhile loopEven and Odd statement......

---
+++ src/loops/Assignment04FebWhile_Loop.java
@@ -0,0 +1,32 @@
+package loops;
+
+public class Assignment04FebWhile_Loop {
+    public static void main(String[] args) {
+        int i;
+        int number;
+        number=100; i=1;
+        while(i<=number){
+            if(i%2==0){
+                System.out.println("Even Num  is : "+i);
+            }else{
+                System.out.println("This is odd no. :" +i);
+            }
+            i++;
+
+        }
+        number=100; i=1;
+        while(i<=number){
+            if(i%2!=0){
+                System.out.println("Odd Num is : "+i);
+            }
+            i++;
+        }
+
+            }
+        }
+
+
+
+
+
+
