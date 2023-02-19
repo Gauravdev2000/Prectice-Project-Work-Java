9668e082e868147bddcbd82567880b07121179d9
dell <gauravdevgd2000@gmail.com>
2023-02-18 21:38:39 -0700

    Scanner Method and calculator rerun and continue

--- src/method_or_function/Scanner_CalculatorApp.java
+++ src/method_or_function/Scanner_CalculatorApp.java
@@ -22,33 +22,42 @@ public class Scanner_CalculatorApp {
 
 
     public static void main(String[] args) {
-        System.out.println("Please enter value of a :");
         Scanner scannerObj= new Scanner(System.in);
-        int a= scannerObj.nextInt();
+        int flag= 0;
+        while(flag==0){
 
-        System.out.println("Please enter value of b : ");
-        int b= scannerObj.nextInt();
-        System.out.println("Please entered your choice : 1 For Addition, 2 For Subtract, 3 For Multiply, 4 For Divide");
-        int choice= scannerObj.nextInt();
+            System.out.println("Please enter value of a :");
 
-        Scanner_CalculatorApp obj= new Scanner_CalculatorApp();
-        if(choice==1){
-            int add= obj.add(a,b);
-            System.out.println("Sum Of : "+add);
-        }
-        else if(choice==2){
-            int sub= obj.subtract(a,b);
-            System.out.println("Subtract is : "+sub);
-        }
-        else if(choice==3){
-            int multi=obj.multiply(a,b);
-            System.out.println("Multiply is : "+multi);
-        }
-        else if(choice==4){
-            double div= obj.divide(a,b);
-            System.out.println("Divide is : "+div);
-        }
+            int a= scannerObj.nextInt();
 
+            System.out.println("Please enter value of b : ");
+            int b= scannerObj.nextInt();
+            System.out.println("Please entered your choice : 1 For Addition, 2 For Subtract, 3 For Multiply, 4 For Divide");
+            int choice= scannerObj.nextInt();
 
+            Scanner_CalculatorApp obj= new Scanner_CalculatorApp();
+            if(choice==1){
+                int add= obj.add(a,b);
+                System.out.println("Sum Of : "+add);
+            }
+            else if(choice==2){
+                int sub= obj.subtract(a,b);
+                System.out.println("Subtract is : "+sub);
+            }
+            else if(choice==3){
+                int multi=obj.multiply(a,b);
+                System.out.println("Multiply is : "+multi);
+            }
+            else if(choice==4){
+                double div= obj.divide(a,b);
+                System.out.println("Divide is : "+div);
+            }
+            Scanner scanner= new Scanner(System.in);
+            System.out.println("Do you want to Continue : Press 0 to continue and Press 1 to exit   ");
+            flag=scanner.nextInt();
+
+        }
     }
+
+
 }
